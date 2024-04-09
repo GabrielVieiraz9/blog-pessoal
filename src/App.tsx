a0b469065fdd685762bd3287ae6e64807820bb95
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-04-09 12:14:43 -0300

    props and hook example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
-
+import React from 'react';
 
 function App() {
   return (
-   <Home />
+    <Home title="Componente Home"
+    description="Este é um componente Home que recebe props."/>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import './Home.css';
+import React, { useState } from 'react';
 
-function Home(){
-    return (
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/d1aJhiB.jpg" alt="Imagem Tela Inicial" className="img"/>
-        </>
-    );
+interface minhaProps {
+  title: string;
+  description: string;
+}
+
+function HomeProps(props: minhaProps) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </>
+  );
+}
+
+function Home(props: minhaProps) {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <div>
+        <h1>Ao clicar nesse botão você verá um exemplo de props</h1>
+      {loggedIn ? (
+        <div>
+          <HomeProps title={props.title} description={props.description} />
+        </div>
+      ) : (
+        <button onClick={() => setLoggedIn(true)}>Exemplo de Hooks</button>
+      )}
+    </div>
+  );
 }
 
 export default Home;
