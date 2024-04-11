35ff0bb6d8eb1b54dd5b4c0ecc8c6ed9aa23f12d
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-04-11 15:58:47 -0300

    login added

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -3,12 +3,13 @@ import { useState, createContext, ReactNode } from 'react';
 interface UserContextType {
   nome: string;
   setNome: React.Dispatch<React.SetStateAction<string>>;
-
+  login: (newNome: string) => void; // Adicionando a função login à interface
 }
 
 export const UserContext = createContext<UserContextType>({
   nome: '',
-  setNome: () => {}
+  setNome: () => {},
+  login: () => {} // Definindo um placeholder para a função login
 });
 
 interface UserProviderProps {
@@ -18,9 +19,12 @@ interface UserProviderProps {
 function UserProvider({ children }: UserProviderProps) {
   const [nome, setNome] = useState('');
 
+  const login = (newNome: string) => {
+    setNome(newNome);
+  };
 
   return (
-    <UserContext.Provider value={{ nome, setNome}}>
+    <UserContext.Provider value={{ nome, setNome, login }}>
       {children}
     </UserContext.Provider>
   );
