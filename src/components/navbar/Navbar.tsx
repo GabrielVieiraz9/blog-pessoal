f00a9853f266a2202f775c0dbb97e2f3ae4b0d7e
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-04-15 18:09:40 -0300

    hax blog named

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Example() {
               </div>
               <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center" style={{ color: 'white', fontFamily: 'Bungee, sans-serif', fontSize: '24px' }}>
-  Blog Pessoal
+              <Link to="/login">Hax Blog</Link>
 </div>
 {usuario.id == 0 && (
                 <div className="hidden sm:ml-6 sm:block pl-10">
