573c2cc3d5c06fb988b94ffe2761d7cab4db0eca
GabrielVieiraz9 <gabrielvieiracontato97@gmail.com>
2024-04-15 17:57:10 -0300

    social media updated (footer)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,23 +1,30 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import {
+  GithubLogo,
+  LinkedinLogo,
+} from "@phosphor-icons/react";
 
 function Footer() {
- 
   return (
     <>
-        <div className="flex justify-center items-end bg-gray-900 text-white">
-          <div className="container flex flex-col items-center py-4">
-          <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
-          <p className='text-lg mb-3'>Acesse nossas redes sociais</p>
-            
-            <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
-            </div>
+      <div className="flex justify-center items-end bg-gray-900 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className="text-xl font-bold">
+            Blog pessoal de Gabriel de Andrade Vieira | Copyright:{" "}
+          </p>
+          <p className="text-lg mb-3">Acesse as minhas redes sociais</p>
+
+          <div className="flex gap-2">
+            <a href="https://www.linkedin.com/in/gabriel-vieira-z9/">
+              <LinkedinLogo size={48} weight="bold" />
+            </a>
+            <a href="https://github.com/GabrielVieiraz9">
+              <GithubLogo size={48} weight="bold" />
+            </a>
           </div>
-          </div>
-      </>
-  )
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
